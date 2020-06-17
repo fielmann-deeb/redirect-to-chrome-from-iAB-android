a85006a27a5ef15a482539ef1152ff82d3403678
Enal Deeb <enal.deeb@fielmann.com>
2020-06-17 17:34:04 +0200

    open current page in chrome

--- main.2.js
+++ main.2.js
@@ -7,4 +7,11 @@ const openCurrentLinkInChrome = function () {
 document.addEventListener("DOMContentLoaded", function (event) {
   const chromeBtn = document.getElementById("open-in-chrome-btn");
   chromeBtn.addEventListener("click", openCurrentLinkInChrome);
+
+  const intentAnchor = document.getElementById("open-in-chrome-intent");
+  intentAnchor.href =
+    "intent://" +
+    window.location.host +
+    window.location.search +
+    "#Intent;scheme=http;action=android.intent.action.VIEW;end;";
 });
